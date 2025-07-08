a6f3a56ce56f8cc302d4789cf82ace0d36e65a9f
juncaroline <caroline_jun@hotmail.com>
2025-07-08 11:19:06 -0300

    testing changes

--- sources/mandatory/parse_elements.c
+++ sources/mandatory/parse_elements.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse_elements.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: cabo-ram <cabo-ram@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 09:26:51 by cabo-ram          #+#    #+#             */
-/*   Updated: 2025/07/06 19:16:36 by marvin           ###   ########.fr       */
+/*   Updated: 2025/07/08 11:17:36 by cabo-ram         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ bool	parse_camera(char **tokens, int count, t_camera *camera)
 	camera->camera_position = parse_coordinates(tokens[1]);
 	camera->camera_direction = parse_normalized_vector(tokens[2]);
 	camera->fov = parse_fov(tokens[3]);
-	if (!is_normalized_vector(dir) || (camera->fov < 0.0
+	if (!is_normalized_vector(camera->camera_direction) || (camera->fov < 0.0
 		|| camera->fov > 180.0))
 		return (false);
 	return (true);
@@ -45,14 +45,13 @@ bool	parse_camera(char **tokens, int count, t_camera *camera)
 
 bool	parse_light(char **tokens, int count, t_light *light)
 {
-	if (count != 4)
+	if (count != 3)
 	{
-		printf("Error: 'L' expects 3 parameters, received %d\n", count - 1);
+		printf("Error: 'L' expects 2 parameters, received %d\n", count - 1);
 		return (false);
 	}
 	light->light_point = parse_coordinates(tokens[1]);
 	light->ratio = parse_ratio(tokens[2]);
-	light->color = parse_rgb(tokens[3]);
 	if (light->ratio < 0.0 || light->ratio > 1.0)
 		return (false);
 	return (true);
